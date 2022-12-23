f304ab02ed2f23719a2f82083ebd6db819448244
mrvauxs <mrvauxs@gmail.com>
2022-12-23 18:39:25 +0000

    1.4.1

--- module/target-damage.js
+++ module/target-damage.js
@@ -2,26 +2,35 @@ Hooks.on("init", async () => {
 	game.settings.register("pf2e-target-damage", "hideNPCs", {
 		scope: "world",
 		config: true,
-		name: "Hide non-Player Token Targets",
-		hint: "Hides not owned by players tokens from the target list of a damage roll.",
+		name: game.i18n.localize("pf2e-target-damage.settings.hideNPCs.name"),
+		hint: game.i18n.localize("pf2e-target-damage.settings.hideNPCs.hint"),
+		type: Boolean,
+		requiresReload: true,
+		default: false
+	});
+	game.settings.register("pf2e-target-damage", "hideTheHidingButtons", {
+		scope: "user",
+		config: true,
+		name: game.i18n.localize("pf2e-target-damage.settings.hideTheHidingButtons.name"),
+		hint: game.i18n.localize("pf2e-target-damage.settings.hideTheHidingButtons.hint"),
 		type: Boolean,
 		requiresReload: true,
 		default: false
 	});
 	game.settings.register("pf2e-target-damage", "hideOGButtons", {
-		scope: "world",
+		scope: "user",
 		config: true,
-		name: "Hide Original PF2e Damage Buttons",
-		hint: "Hides original PF2e system damage buttons for those from Damage Target.",
+		name: game.i18n.localize("pf2e-target-damage.settings.hideOGButtons.name"),
+		hint: game.i18n.localize("pf2e-target-damage.settings.hideOGButtons.hint"),
 		type: Boolean,
 		requiresReload: true,
 		default: false
 	});
 	game.settings.register("pf2e-target-damage", "persistentDamageInt", {
-		scope: "world",
+		scope: "user",
 		config: true,
-		name: "Integrate with Persistent Damage",
-		hint: "Hides the original PF2e system damage buttons for those from Damage Target, but only for the PF2e Persistent Damage damage rolls.\nIs overriden by 'Hide Original PF2e Damage Buttons' setting.",
+		name: game.i18n.localize("pf2e-target-damage.settings.persistentDamageInt.name"),
+		hint: game.i18n.localize("pf2e-target-damage.settings.persistentDamageInt.hint"),
 		type: Boolean,
 		requiresReload: true,
 		default: true
