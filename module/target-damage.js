39c55a89463daab98504f88d2e9cd537590691c0
mrvauxs <mrvauxs@gmail.com>
2023-04-12 01:01:08 +0000

    4.10.1

--- module/target-damage.js
+++ module/target-damage.js
@@ -280,6 +280,36 @@ function updateMessageWithFlags(message, event) {
 	});
 }
 
+async function extractEphemeralEffects({
+	affects,
+	origin,
+	target,
+	item,
+	domains,
+	options
+}) {
+	if (!(origin && target)) return []
+
+	const [effectsFrom, effectsTo] =
+		affects === "target" ? [origin, target] : [target, origin]
+	const fullOptions = [...options, ...effectsTo.getSelfRollOptions(affects)]
+	const resolvables = item
+		? item.isOfType("spell")
+			? { spell: item }
+			: { weapon: item }
+		: {}
+	return (
+		await Promise.all(
+			domains
+				.flatMap(
+					s => effectsFrom.synthetics.ephemeralEffects[s]?.[affects] ?? []
+				)
+				.map(d => d({ test: fullOptions, resolvables }))
+		)
+	).flatMap(e => e ?? [])
+}
+
+
 async function applyDamage(message, tokenID, multiplier, addend = 0, promptModifier = false, rollIndex = 0) {
 	if (promptModifier) return shiftModifyDamage(message, tokenID, multiplier, rollIndex);
 	// Modified here to include TokenID
